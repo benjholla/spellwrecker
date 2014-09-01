7097076709b1a753552821255acd050ac1324df4
benjholla <benjholla@gmail.com>
2014-08-31 21:12:03 -0500

    Added a monitor, but not super happy with the full effect yet

--- SpellWrecker/src/spellwrecker/monitors/Monitor.java
+++ SpellWrecker/src/spellwrecker/monitors/Monitor.java
@@ -1,13 +1,46 @@
 package spellwrecker.monitors;
 
+import java.util.LinkedList;
+
 public class Monitor {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private long timeDelta;
+	private LinkedList<Long> observations = new LinkedList<Long>();
+//	private LinkedList<Integer> history = new LinkedList<Integer>();
+	private int maxObservations = 0;
+//	private double averageObservations = 0;
+	
+	public Monitor(long timeDelta){
+		this.timeDelta = timeDelta;
 	}
-
+	
+	public void observe(){
+		long currentTimestamp = System.currentTimeMillis();
+		observations.add(currentTimestamp);
+		long expirationTimestamp = currentTimestamp - timeDelta;
+		while(true){
+			if(observations.getFirst() <= expirationTimestamp){
+				observations.removeFirst();
+			} else {
+				break;
+			}
+		}
+		int numObservations = observations.size();
+		if(numObservations > maxObservations){
+			maxObservations = numObservations;
+		}
+	}
+	
+	public int getMaxObservations(){
+		return maxObservations;
+	}
+	
+	public int getObservations(){
+		return observations.size();
+	}
+	
+	public void resetMaxObservations(){
+		maxObservations = 0;
+		observations.clear();
+	}
 }
---
+++ SpellWrecker/src/spellwrecker/test/TestMonitor.java
@@ -0,0 +1,85 @@
+package spellwrecker.test;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
+import spellwrecker.monitors.Monitor;
+import spellwrecker.spellwreckers.QwertySpellWrecker;
+
+public class TestMonitor {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestMonitor window = new TestMonitor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TestMonitor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		final JTextArea monitoredTextArea = new JTextArea();
+		monitoredTextArea.setWrapStyleWord(true);
+		frame.getContentPane().add(monitoredTextArea);
+		
+		final JLabel keystrokesPerUnitLabel = new JLabel("Keystrokes Per Second: ");
+		keystrokesPerUnitLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(keystrokesPerUnitLabel, BorderLayout.NORTH);
+		
+		final Monitor monitor = new Monitor(1000);
+		final Random rnd = new Random();
+		
+		monitoredTextArea.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				monitor.observe();
+				keystrokesPerUnitLabel.setText("Keystrokes Per Second: " + monitor.getObservations() 
+						+ ", Max: " + monitor.getMaxObservations());
+				
+				if(monitor.getObservations() > 6 && monitor.getObservations() == monitor.getMaxObservations()){
+					if(rnd.nextInt(4) == 0){
+						e.setKeyChar(QwertySpellWrecker.spellwreck(e.getKeyChar()));
+						monitor.resetMaxObservations();
+						keystrokesPerUnitLabel.setForeground(Color.RED);
+					}
+				} else {
+					keystrokesPerUnitLabel.setForeground(Color.BLACK);
+				}
+			}
+		});
+	}
+
+}
